5af12c8a8b042bb3f8a9abc38a818542322a9ea0
Alessandro Crinella <alecrinella99@gmail.com>
2022-10-23 18:38:04 +0200

    Create game start logic
    Now the user can choose the sign they want to play with and the opponent
    which can be a real player or the cpu.
    
    States are temporarily managed with useState

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import { GameContainer } from "./components/GameContainer";
 import { StartModal } from "./components/StartModal";
 
 function App() {
+  const [hoveringPick, setHoveringPick] = useState("x");
+  const [player1, setPlayer1] = useState({ player: "player", sign: "x" });
+  const [player2, setPlayer2] = useState({ player: "", sign: "" });
+
+  const handleHoveringPick = () => {
+    setHoveringPick((prev) => (prev === "x" ? "o" : "x"));
+  };
+
+  const handleSetPlayerPick = (opponent) => {
+    setPlayer1((prev) => ({ ...prev, sign: hoveringPick }));
+    setPlayer2({ player: opponent, sign: hoveringPick === "x" ? "o" : "x" });
+  };
+
   return (
     <>
-      {/*<StartModal />*/}
-
-      <GameContainer />
+      {!player2.sign && (
+        <StartModal
+          setHoveringPick={handleHoveringPick}
+          setPick={handleSetPlayerPick}
+          hoveringPick={hoveringPick}
+        />
+      )}
+      {player2.sign && <GameContainer />}
     </>
   );
 }
--- tictactoe/src/components/StartModal.jsx
+++ tictactoe/src/components/StartModal.jsx
@@ -1,20 +1,45 @@
 import React from "react";
 import classes from "./styles/StartModal.module.css";
 
-export function StartModal({}) {
+export function StartModal({ setPick, setHoveringPick, hoveringPick }) {
+  const handleSetPicks = (e) => {
+    const opponent = e.target.getAttribute("data-player");
+    setPick(opponent);
+  };
+
   return (
     <div className={classes.start}>
       <div className={classes["choose-container"]}>
         <p>PICK PLAYER 1'S MARK</p>
         <div className={classes.chooses}>
-          <div className={classes.active}>X</div>
-          <div className="">O</div>
+          <div
+            className={`${hoveringPick === "x" ? classes.active : ""}`}
+            onClick={setHoveringPick}
+          >
+            X
+          </div>
+          <div
+            className={`${hoveringPick === "o" ? classes.active : ""}`}
+            onClick={setHoveringPick}
+          >
+            O
+          </div>
         </div>
         <p>REMEMBER: X GOES FIRST</p>
       </div>
       <div className={classes["opponent-container"]}>
-        <button className={classes["yellow-button"]}>NEW GAME (VS CPU)</button>
-        <button className={classes["light-blue-button"]}>
+        <button
+          data-player="player"
+          className={classes["yellow-button"]}
+          onClick={handleSetPicks}
+        >
+          NEW GAME (VS CPU)
+        </button>
+        <button
+          data-player="cpu"
+          className={classes["light-blue-button"]}
+          onClick={handleSetPicks}
+        >
           NEW GAME (VS PLAYER)
         </button>
       </div>
